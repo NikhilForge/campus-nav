8e38d7e2f53b1b7ee7a58afcea315aa4edbd325c
Nikhil Sajan <nikhilsajann@gmail.com>
2025-12-15 22:07:01 +0530

    Fix multi-user Firebase data isolation and auth init

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA8Cym5VZDWrXTp5EFvjNzyMLze_rmihnQ",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// EXPORT AUTH (THIS WAS MISSING)
+// EXPORT AUTH + FIRESTORE
 export const auth = getAuth(app);
+export const db = getFirestore(app);
